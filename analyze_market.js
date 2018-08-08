c84c50d45975c9cc30c38b84c71b9aa6ef63617f
roberto-waybetter <roberto@waybetter.com>
2018-08-08 16:38:46 -0400

    Analyze Markets

--- analyze_market.js
+++ analyze_market.js
@@ -3,33 +3,50 @@ var options = {
   port: '3000'
 };
 const rest = require("./http-rest.js");
+const exchange_data = require("./exchanges.json");
+const fs = require('fs');
 
-module.exports = async function(market, exchanges){
+module.exports = async function(market, exchanges) {
   var price_data = [];
-  for(var i=0; i<exchanges.length; i++){
+  for (var i = 0; i < exchanges.length; i++) {
+    console.log("MKT: "+ market)
     options.path = "/markets/get_ticker?market=" + market + "&exchange=" + exchanges[i];
     console.log(options);
-    price_data.push({"exchange":exchanges[i], "last": parseFloat((await rest(options)).last), "timestamp": new Date()});
+    price_data.push({
+      "exchange": exchanges[i],
+      "last": parseFloat((await rest(options)).last),
+      "timestamp": new Date()
+    });
   }
 
   var arbitrage_data = [];
-  for(var i=0; i<price_data.length-1; i++){
-    for(var j=i+1; j<price_data.length; j++){
+  for (var i = 0; i < price_data.length - 1; i++) {
+    for (var j = i + 1; j < price_data.length; j++) {
       var low_index;
       var high_index;
-      if(price_data[i].last < price_data[j].last){
+      if (price_data[i].last < price_data[j].last) {
         low_index = i;
         high_index = j;
       } else {
         low_index = j;
         high_index = i;
       }
+      var low_data = price_data[low_index];
+      var high_data = price_data[high_index];
+    /*  //This needs to be cut down
+      low_data.fees = exchange_data[low_data.exchange].fees;
+      low_data.limits = exchange_data[low_data.exchange].limits;
+      high_data.fees = exchange_data[high_data.exchange].fees;
+      high_data.limits = exchange_data[high_data.exchange].limits;*/
+
       arbitrage_data.push({
-        "low": price_data[low_index],
-        "high": price_data[high_index],
+        "market": market,
+        "low": low_data,
+        "high": high_data,
         "timestamp": Math.min(price_data[low_index].timestamp, price_data[high_index].timestamp),
-        "difference": price_data[high_index].last/price_data[low_index].last - 1
+        "difference": price_data[high_index].last / price_data[low_index].last - 1
       });
+
     }
   }
   console.log(arbitrage_data);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const init = require("./init.js");
 const analyze = require("./analyze_market.js");
+const fs = require('fs');
 var exchanges_by_market = require("./data/exchanges_by_market.json");
 console.log("Opportunity Finder")
 console.log(exchanges_by_market);
@@ -18,5 +19,12 @@ async function run(){
       break;
     }
   }
+  fs.writeFile('./data/arbitrage_opps.json', JSON.stringify(arbitrage_data), 'utf8', function (err) {
+      if (err) {
+          return console.log(err);
+      }
+
+      console.log("exchanges_by_market.json has been updated");
+  });
   console.log(arbitrage_data);
 }
